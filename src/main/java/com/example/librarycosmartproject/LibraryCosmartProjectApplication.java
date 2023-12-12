8bb438f2f41b2e749051acdc3352599934452c83
Muhammad Farhan <farhan.syahputra@doku.com>
2023-12-12 14:13:57 +0700

    init project

---
+++ src/main/java/com/example/librarycosmartproject/LibraryCosmartProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarycosmartproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCosmartProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryCosmartProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarycosmartproject/LibraryCosmartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarycosmartproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCosmartProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
